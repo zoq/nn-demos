32cfafe9090e6814677a463d851add94c957852e
Marcus Edel <marcus.edel@fu-berlin.de>
2015-07-24 13:13:26 +0200

    Add setting file.

---
+++ config.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file config.hpp
+ * @author Marcus Edel
+ *
+ * Miscellaneous settings.
+ */
+
+#include <iostream>
+#include <string>
+
+namespace config {
+  // The deployment domain and path.
+  const static std::string domain = "mnist";
+}
