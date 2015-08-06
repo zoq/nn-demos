293ef3202de982524b9076b7f19f781dfd6b920e
Marcus Edel <marcus.edel@fu-berlin.de>
2015-08-06 21:17:12 +0200

    Add deployment url.

--- config.hpp
+++ config.hpp
@@ -12,8 +12,11 @@ namespace config {
   // The deployment path.
   const static std::string path = "mnist";
 
+  // The websocket port.
+  const static int port = 9972;
+
   // The deployment url.
-  const static std::string url = "https://urgs.org/mnist";
+  const static std::string url = "ws://urgs.org:9972/mnist";
 
   // The dataset to be loaded.
   const static std::string dataset = "/home/marcus/src/mlpack/build/mnist_first250_training_4s_and_9s.arm";
